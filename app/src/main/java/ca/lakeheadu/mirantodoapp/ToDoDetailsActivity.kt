f6f47a79a20e7dbe8d0613a0befa49dd3fde0efd
Miran Qarachatani <miranararat722@gmail.com>
2024-03-22 23:55:11 -0400

    Set minDate for calender in items without due date

--- app/src/main/java/ca/lakeheadu/mirantodoapp/ToDoDetailsActivity.kt
+++ app/src/main/java/ca/lakeheadu/mirantodoapp/ToDoDetailsActivity.kt
@@ -22,16 +22,20 @@ class ToDoDetailsActivity : AppCompatActivity() {
         binding.statusEditText.setText(if (isDone) "Completed" else "Not Completed")
 
         binding.notesEditText.setText(intent.getStringExtra("notes"));
-
+        val today = Calendar.getInstance().timeInMillis
         if (dueDateMillis != -1L) {
             binding.calendarView.date = dueDateMillis
             // can't set up a to do for the past
-                val today = Calendar.getInstance().timeInMillis
+
             // only if the due date in the future if it's in the past then you can reset to past
                 if(dueDateMillis>today){
                     binding.calendarView.minDate = today
                 }
             }
+        else{
+            // if due date not set still limit
+            binding.calendarView.minDate = today
+        }
 
         setupClickListeners()
     }
