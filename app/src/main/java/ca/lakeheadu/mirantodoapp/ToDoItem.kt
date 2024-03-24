7112f9d4c3fff86ca271434e0f3ad1307374cce5
Miran Qarachatani <miranararat722@gmail.com>
2024-03-24 14:43:23 -0400

    Add documentation for data class

--- app/src/main/java/ca/lakeheadu/mirantodoapp/ToDoItem.kt
+++ app/src/main/java/ca/lakeheadu/mirantodoapp/ToDoItem.kt
@@ -2,6 +2,9 @@ package ca.lakeheadu.mirantodoapp
 
 import java.time.LocalDate
 
+/**
+ * This data class defines a to do item, it has 4 properties and stores date ad a LocaleDate
+ */
 data class ToDoItem(
     val title:String,
     var isDone: Boolean,
